b4ff3c677edb27e3c9cbf33b09bb72b5dcb4be83
Seanghay Yath <seanghay.dev@gmail.com>
2025-06-21 17:29:07 +0700

    add skia-canvas

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,11 @@
-export function GET(request) {
-  return new Response('Hello from Vercel!');
+import { Canvas } from "skia-canvas";
+
+export async function GET(request) {
+	const canvas = new Canvas(512, 512);
+	const ctx = canvas.getContext("2d");
+	ctx.fillStyle = "red";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	const buffer = await canvas.toBuffer();
+
+	return new Response(buffer);
 }
